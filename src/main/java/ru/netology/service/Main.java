c3704b27b0940f45bf2bf0df84057726667e66b3
Andrey Malov <maa.pers@gmail.com>
2022-08-09 20:17:47 +0200

    Main class added

---
+++ src/main/java/ru/netology/service/Main.java
@@ -0,0 +1,9 @@
+package ru.netology.service;
+
+public class Main {
+    public static void main(String[] args) {
+        CashbackService service = new CashbackService();
+        int result = service.remain(1800);
+        System.out.println("For more cashback please add a purchase in the amount of " + result);
+    }
+}
